d877ed47e308d9fe8a0c7596445173210dd17953
Lais Godinho <47223072+laisbastosbg@users.noreply.github.com>
2025-12-15 18:22:51 -0300

    Initial Commit

---
+++ POCMultipleViewsSwiftDataMVVM/POCMultipleViewsSwiftDataMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCMultipleViewsSwiftDataMVVMApp.swift
+//  POCMultipleViewsSwiftDataMVVM
+//
+//  Created by Lais Godinho on 15/12/25.
+//
+
+import SwiftUI
+
+@main
+struct POCMultipleViewsSwiftDataMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ POCMultipleViewsSwiftDataMVVMTests/POCMultipleViewsSwiftDataMVVMTests.swift
@@ -0,0 +1,17 @@
+//
+//  POCMultipleViewsSwiftDataMVVMTests.swift
+//  POCMultipleViewsSwiftDataMVVMTests
+//
+//  Created by Lais Godinho on 15/12/25.
+//
+
+import Testing
+@testable import POCMultipleViewsSwiftDataMVVM
+
+struct POCMultipleViewsSwiftDataMVVMTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
